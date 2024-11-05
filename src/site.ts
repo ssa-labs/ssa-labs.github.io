339848625dd08f3648f672f7767906afa828b43a
keyxyz <135964407+keyxyz@users.noreply.github.com>
2024-11-05 13:50:33 +0300

    update metadata, logo and font

---
+++ src/site.ts
@@ -0,0 +1,6 @@
+export const BRAND = 'SSA Labs - Rapid Innovation, Tailored Just for You'
+export const DESCRIPTION = 'SSA Labs offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!'
+export const WEBSITE = 'https://ssa-labs.github.io'
+export const FAVICON = 'https://ssa-labs.github.io/assets/favicon.svg'
+export const OGIMAGE = 'https://ssa-labs.github.io/assets/ogimage.png'
+export const LOGO = 'https://ssa-labs.github.io/assets/logo.svg'
