19653f34b81f68e23ee864fd821a8a8c822aca31
keyxyz <135964407+keyxyz@users.noreply.github.com>
2024-11-05 14:30:53 +0300

    update site details

--- src/site.ts
+++ src/site.ts
@@ -1,6 +1,9 @@
+export const SITE = 'SSA Labs'
 export const BRAND = 'SSA Labs - Rapid Innovation, Tailored Just for You'
 export const DESCRIPTION = 'SSA Labs offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!'
 export const WEBSITE = 'https://ssa-labs.github.io'
 export const FAVICON = 'https://ssa-labs.github.io/assets/favicon.svg'
 export const OGIMAGE = 'https://ssa-labs.github.io/assets/ogimage.png'
 export const LOGO = 'https://ssa-labs.github.io/assets/logo.svg'
+
+export const SERVICES = "At SSA Labs, we specialize in software development and computer maintenance that transform your business and elevate your technology experience.<br>Every project is more than just a service; it's an opportunity to create something exceptional.<br>You envision it, and we'll build it."
