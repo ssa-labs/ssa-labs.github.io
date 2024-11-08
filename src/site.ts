4518490fe9434434d72c02a34fa11dcbe7f03382
keyxyz <135964407+keyxyz@users.noreply.github.com>
2024-11-08 14:52:43 +0000

    add code formatter and linter

--- src/site.ts
+++ src/site.ts
@@ -1,10 +1,11 @@
-export const SITE = 'SSA Labs'
-export const BRAND = 'SSA Labs - Rapid Innovation, Tailored Just for You'
-export const DESCRIPTION = 'SSA Labs offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!'
-export const WEBSITE = 'https://ssa-labs.github.io'
-export const FAVICON = 'https://ssa-labs.github.io/assets/favicon.svg'
-export const OGIMAGE = 'https://ssa-labs.github.io/assets/ogimage.png'
-export const LOGO = 'https://ssa-labs.github.io/assets/logo.svg'
+export const SITE = "SSA Labs";
+export const BRAND = "SSA Labs - Rapid Innovation, Tailored Just for You";
+export const DESCRIPTION =
+	"SSA Labs offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!";
+export const WEBSITE = "https://ssa-labs.github.io";
+export const FAVICON = "https://ssa-labs.github.io/assets/favicon.svg";
+export const OGIMAGE = "https://ssa-labs.github.io/assets/ogimage.png";
+export const LOGO = "https://ssa-labs.github.io/assets/logo.svg";
 
-
-export const SERVICES = "Our company specializes in software development and computer maintenance."
+export const SERVICES =
+	"Our company specializes in software development and computer maintenance.";
