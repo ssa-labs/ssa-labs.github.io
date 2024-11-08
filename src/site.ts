7e310536a7be29e4cc56d1160c15923a625d721d
keyxyz <135964407+keyxyz@users.noreply.github.com>
2024-11-08 16:36:50 +0000

    update footer, images, socials

--- src/site.ts
+++ src/site.ts
@@ -1,5 +1,6 @@
 export const SITE = "SSA Labs";
-export const BRAND = "SSA Labs - Rapid Innovation, Tailored Just for You";
+export const SLOGAN = "Rapid Innovation, Tailored Just for You";
+export const BRAND = `${SITE} - ${SLOGAN}`;
 export const DESCRIPTION =
 	"SSA Labs offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!";
 export const WEBSITE = "https://ssa-labs.github.io";
