af31da238b834873c2f1a490f7cdf740ac45082d
keyxyz <135964407+keyxyz@users.noreply.github.com>
2024-11-09 09:32:12 +0000

    fix page bugs

--- src/site.ts
+++ src/site.ts
@@ -2,7 +2,8 @@ export const SITE = "SSA Labs";
 export const SLOGAN = "Rapid Innovation, Tailored Just for You";
 export const BRAND = `${SITE} - ${SLOGAN}`;
 export const DESCRIPTION =
-	"SSA Labs offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!";
+	`${SITE} offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!`;
+
 export const WEBSITE = "https://ssa-labs.github.io";
 export const FAVICON = "https://ssa-labs.github.io/assets/favicon.svg";
 export const OGIMAGE = "https://ssa-labs.github.io/assets/ogimage.png";
