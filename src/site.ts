53dcc1fdb5fe138a81582e7371b02cc3f50c1081
keyxyz <135964407+keyxyz@users.noreply.github.com>
2024-11-09 12:19:49 +0000

    update booking link

--- src/site.ts
+++ src/site.ts
@@ -1,13 +1,16 @@
 export const SITE = "SSA Labs";
+export const EMAIL = "#";
 export const SLOGAN = "Rapid Innovation, Tailored Just for You";
 export const BRAND = `${SITE} - ${SLOGAN}`;
 export const DESCRIPTION =
 	`${SITE} offers tailored software development and reliable computer maintenance services, empowering businesses to enhance efficiency and performance. Partner with us for innovative tech solutions!`;
 
 export const WEBSITE = "https://ssa-labs.github.io";
-export const FAVICON = "https://ssa-labs.github.io/assets/favicon.svg";
-export const OGIMAGE = "https://ssa-labs.github.io/assets/ogimage.png";
-export const LOGO = "https://ssa-labs.github.io/assets/logo.svg";
+export const FAVICON = `${WEBSITE}/assets/favicon.svg`;
+export const OGIMAGE = `${WEBSITE}/assets/ogimage.png`;
+export const LOGO = `${WEBSITE}/assets/logo.svg`;
 
 export const SERVICES =
 	"Our company specializes in software development and computer maintenance.";
+
+export const BOOKING = "/contact"
